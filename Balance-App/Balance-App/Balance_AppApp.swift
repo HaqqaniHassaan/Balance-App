af1a65d967910fa42495691be7cbaa14c833a7b4
Hassaan Haqqani <haqqani.hassaan@gmail.com>
2024-10-23 18:09:10 -0700

    Splash screen/app icon added

--- Balance-App/Balance-App/Balance_AppApp.swift
+++ Balance-App/Balance-App/Balance_AppApp.swift
@@ -1,23 +1,29 @@
-//
-//  Balance_AppApp.swift
-//  Balance-App
-//
-//  Created by user264048 on 10/21/24.
-//
-
 import SwiftUI
 
 @main
 struct Balance_AppApp: App {
     // Create a shared instance of CoreDataViewModel
     @StateObject private var coreDataViewModel = CoreDataViewModel()
+    @State private var showSplash = true // State to manage splash visibility
 
     var body: some Scene {
         WindowGroup {
-            if coreDataViewModel.isOnboardingCompleted() {
-                ContentView(coreDataViewModel: coreDataViewModel) // Pass to ContentView
+            if showSplash {
+                // Pass coreDataViewModel to SplashScreenView
+                SplashScreenView(coreDataViewModel: coreDataViewModel)
+                    .onAppear {
+                        // Delay transition to the main flow
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                            showSplash = false
+                        }
+                    }
             } else {
-                OnboardingWelcomeView(coreDataViewModel: coreDataViewModel) // Pass to Onboarding
+                // Check if onboarding is completed
+                if coreDataViewModel.isOnboardingCompleted() {
+                    ContentView(coreDataViewModel: coreDataViewModel)
+                } else {
+                    OnboardingWelcomeView(coreDataViewModel: coreDataViewModel)
+                }
             }
         }
     }
