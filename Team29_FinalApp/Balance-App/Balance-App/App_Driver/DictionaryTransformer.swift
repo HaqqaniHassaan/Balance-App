f68b713035bb1d73c156c915f18531ff209a051a
HaqqaniHassaan <157773470+HaqqaniHassaan@users.noreply.github.com>
2025-01-27 12:26:13 -0600

    Delete outdated directory
    Was used for final sem project, Team29 refers to my team (which consists of only me)

--- Team29_FinalApp/Balance-App/Balance-App/App_Driver/DictionaryTransformer.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DictionaryTransformer.swift
-//  Balance-App
-//
-//  Created by user264048 on 12/4/24.
-//
-
-
-import Foundationclass DictionaryTransformer: ValueTransformer {    override func transformedValue(_ value: Any?) -> Any? {        guard let dictionary = value as? [Int: Double] else { return nil }        return try? JSONEncoder().encode(dictionary)    }    override func reverseTransformedValue(_ value: Any?) -> Any? {        guard let data = value as? Data else { return nil }        return try? JSONDecoder().decode([Int: Double].self, from: data)    }    static func register() {        let transformerName = NSValueTransformerName("DictionaryTransformer")        ValueTransformer.setValueTransformer(DictionaryTransformer(), forName: transformerName)    }}
